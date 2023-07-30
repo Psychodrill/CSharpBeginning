ae4fe03f263bf0d298c9268dcabaa009f987a8b9
Zhiganov Taras <zhiganovtaras@.gmail.com>
2023-07-30 15:13:25 +0300

    SortMethod added

---
+++ SortMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+void OrderedArray(int []array)
+{
+    for(int i=0; i< array.Length-1; i++)
+    {
+        int minPosition=i;
+        for(int j=i+1; j<array.Length; j++)
+        {
+            if(array[j]<array[minPosition])
+            {
+                minPosition=j;
+            }
+        }
+        int temporary = array[i];
+        array[i]= array[minPosition];
+        array[minPosition]=temporary;
+    }
+    
+}
+void PrintArray(int [] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+
+}
+
+int [] testArray = {4,67,3,9,0,4,55,34,71,32,58};
+PrintArray(testArray);
+OrderedArray(testArray);
+PrintArray(testArray);
